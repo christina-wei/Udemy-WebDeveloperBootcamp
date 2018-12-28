2cc6a5289fc0738ac5b74b70c9dc0dd9bdccfa3d
Christina Wei <45274683+christina-wei@users.noreply.github.com>
2018-12-28 11:48:59 -0500

    Add files via upload

--- NodeJS/DemoApp/app.js
+++ NodeJS/DemoApp/app.js
@@ -1,1 +1,9 @@
+//Using Cat-Me package
+var catMe = require("cat-me");
 
+console.log("FROM APP.JS");
+console.log(catMe());
+
+//Using Knock-Knock jokes package
+var knock = require("knock-knock-jokes");
+console.log(knock());
