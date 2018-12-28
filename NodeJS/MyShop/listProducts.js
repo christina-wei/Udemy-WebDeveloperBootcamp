ff22c4ad99a33d74362b78d00ec8d2315af018b8
Christina Wei <45274683+christina-wei@users.noreply.github.com>
2018-12-28 11:52:08 -0500

    Add files via upload

--- NodeJS/MyShop/listProducts.js
+++ NodeJS/MyShop/listProducts.js
@@ -1,1 +1,12 @@
+//import packages
+var faker = require("faker");
 
+//welcome message
+console.log("=====================");
+console.log("WELCOME TO MY SHOP:")
+console.log("=====================");
+
+//loop through and get 10 random product & prices
+for (var i = 0; i < 10; i++) {
+	console.log(faker.commerce.productName() + " - $" + faker.commerce.price());
+}
